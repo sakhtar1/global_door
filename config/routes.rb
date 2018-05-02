49ea5a701afd61ab1aa5596d0a568475a2273873
Sharmina Akhtar <sakht318@gmail.com>
2018-05-02 20:10:28 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :users
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+  root 'users#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
