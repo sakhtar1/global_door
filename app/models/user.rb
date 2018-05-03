b358bc4481eaaa586a104bbcaee6589cc54fe753
github username <github email address>
2018-05-03 15:41:54 -0400

    check

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :continents
   has_many :countries
-  has_many :cities,through: :countries
+  has_many :cities, through: :countries
 
 end
