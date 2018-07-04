3a1842ef26d3667e86c417d914e809062fe59e51
github username <github email address>
2018-07-04 23:41:15 +0300

    city issue

--- db/migrate/20180514193337_create_countries.rb
+++ db/migrate/20180514193337_create_countries.rb
@@ -2,7 +2,7 @@ class CreateCountries < ActiveRecord::Migration[5.2]
   def change
     create_table :countries do |t|
     	t.string :name
-      	t.string :country
+      	t.string :city
       	t.text :reason
     end
   end
