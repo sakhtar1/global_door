abe34f39fef6852f71bb330119cbe77fa2355661
github username <github email address>
2018-07-07 08:45:39 +0300

    form

--- app/controllers/continents_controller.rb
+++ app/controllers/continents_controller.rb
@@ -2,12 +2,28 @@ class ContinentsController < ApplicationController
 	skip_before_action :user_authenticated, only: [:index,:north_america, :south_america, :asia, :europe, :africa, :antartica, :australia]
 	
 	def index
-		@continent = Continent.all
 	end
 
 	def new
-		@continent = Continent.new
+		@continent = Continent.new(continent_params)
+       if @continent.save
+    
+        	redirect_to :show
+
+    	else
+      		redirect_to :new 
+    	end
 	end
+
+	def show
+		@country = Continent.find_by(id:params[:id])
+	end
+
+	def destroy
+	    @country.destroy
+	    redirect_to :continents, notice: 'Country was successfully destroyed.'
+    end
+
 
 	def north_america
 	end
@@ -30,4 +46,16 @@ class ContinentsController < ApplicationController
 	def australia
 	end
 
+  private
+    def country_params
+      params.require(:continent).permit(
+        :continent_name,
+        :country_name,
+        :description
+        )
+    end
+
+
 end
+
+
